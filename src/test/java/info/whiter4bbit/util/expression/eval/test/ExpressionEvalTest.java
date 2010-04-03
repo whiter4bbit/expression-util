33121ed7b2a8177b8740ab4c94df317eeccc4b3c
whiter4bbit <wr4bbit@gmail.com>
2010-04-03 19:48:46 +0300

    added tests

--- src/test/java/info/whiter4bbit/util/expression/eval/test/ExpressionEvalTest.java
+++ src/test/java/info/whiter4bbit/util/expression/eval/test/ExpressionEvalTest.java
@@ -8,7 +8,6 @@ public class ExpressionEvalTest extends TestCase{
 	public void testCalc() throws Exception{
 		ExpressionEval eval = new ExpressionEval();
 
-		long startTime = System.currentTimeMillis();
 		Object val = eval.eval("2+2*2");
 		assertEquals(6L, val);
 		
@@ -17,7 +16,22 @@ public class ExpressionEvalTest extends TestCase{
 		
 		val = eval.eval("10*10-1000*300+10/2+300/10-200*500/1234-4444444*2222222*1111");
 		assertEquals(-10972837311904994L, val);
-		System.out.println(System.currentTimeMillis()-startTime);
+	}
+	
+	public void testVars() throws Exception{
+		ExpressionEval eval = new ExpressionEval();
+		eval.setVariable("expr", 100.);
+		eval.setVariable("expr2", 200.);
+		
+		Object val = eval.eval("${expr}+${expr2}");
+		assertEquals(300., val);
+		
+		eval.setVariable("expr", "mama");
+		eval.setVariable("expr2", "myla");
+		eval.setVariable("expr3", "ramu");
+		
+		val = eval.eval("${expr}&${expr2}&${expr3}");
+		assertEquals("mamamylaramu", val);
 	}
 	
 }
